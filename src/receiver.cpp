27585bdbf83039b9765c107a351c85a09890c880
beb44 <blaine@numericable.fr>
2018-01-18 11:11:57 +0100

    make coding convention compliant

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 #include <bitset>
 #include <thread>
-#include "receiver.h"
+#include "receiver.h" 
 
 using namespace std;
- 
+#if 0 
 receiver::receiver()
 {  
-  peer = (elink *)0;
-  issync = false;
-  syncpos = 0;
-  synchead = sampa_head().build_sync();
-  curhead = 0;
+  m_peer = (elink *)0;
+  m_is_synced = false;
+  m_syncpos = 0;
+  m_synchead = sampa_head().build_sync();
+  m_curhead = 0;
   user_handler = 0;
   rec_handl = 0;
 #ifdef STATS
   reset_stats();
 #endif
 }
-
-receiver::receiver(elink *p)
+#endif
+receiver::receiver(elink &p) : m_peer(p)
 {  
-  peer = p;
-  issync = false;
-  synchead = sampa_head().build_sync();
-  syncpos = 0;
-  curhead = 0;
+  m_is_synced = false;
+  m_synchead = sampa_head().build_sync();
+  m_syncpos = 0;
+  m_curhead = 0;
   user_handler = 0;
   rec_handl = 0;
 #ifdef STATS
@@ -33,12 +32,11 @@ receiver::receiver(elink *p)
 #endif
 }
 
-receiver::receiver(int port,GBT_r *p)
+receiver::receiver(int port,GBT_r &p): m_peer(p.get_elink(port)), m_port(port) 
 {
-  peer = p->get_elink(port);
-  synchead = sampa_head().build_sync();
-  syncpos = 0;
-  curhead = 0;
+  m_synchead = sampa_head().build_sync();
+  m_syncpos = 0;
+  m_curhead = 0;
   user_handler = 0;
   rec_handl = 0;
 #ifdef STATS
@@ -51,8 +49,7 @@ void receiver::start()
   try {
   TheThread = new std::thread(&receiver::process,this);
   }
-  catch (const std::exception& e)
-  {
+  catch (const std::exception& e) {
     cout << "execption catched" << endl;
     throw;
   }
@@ -99,82 +96,69 @@ sampa_head  head_decoder(header);
 }
 void receiver::process()
 {  
-  if (peer == (elink *)0) return;
-  while (peer->serial_available()) 
-  {
-    if (issync)
-    {
-      if (headcd !=0)
-      {
-        curhead = ((curhead >>1) + (((uint64_t)(peer->get_serial()))<<49)) & 0x3ffffffffffff;
-        headcd --;
-	if (headcd ==0)
-	{ 
+int      payload_length;
+
+//  if (m_peer == (elink *)0) return;
+  while (m_peer.serial_available()) {
+    if (m_is_synced) {
+      if (m_headcd !=0) {
+        m_curhead = ((m_curhead >>1) + (((uint64_t)(m_peer.get_serial()))<<49)) & 0x3ffffffffffff;
+        m_headcd --;
+	if (m_headcd ==0) { 
 	  // header fully loaded, get playload length
-	  payload_length = sampa_head().get_nbwords(curhead); 
+	  payload_length = sampa_head().get_nbwords(m_curhead); 
 #ifdef STATS
-          packetcount_by_type[sampa_head().get_packet_type(curhead)]++;
+          packetcount_by_type[sampa_head().get_packet_type(m_curhead)]++;
 	  packetcount++;
 #endif	
-          cout << syncpos << " "<< std::bitset<50>( curhead ) << "payload length " << payload_length << endl;
-	  if (payload_length == 0)
-	  {
+          cout << m_syncpos << " "<< std::bitset<50>( m_curhead ) << "payload length " << payload_length << endl;
+	  if (payload_length == 0) {
 	    // zero length packet, catch next header
-	    headcd = 50; // 50 bit of header to be read
+	    m_headcd = 50; // 50 bit of header to be read
 	  }
-	  else
-	  {
+	  else {
 	    // non empty packet, process
-	    _wpointer = &_frame[0];
-            *_wpointer = 0;
-	    cur_len = 0; cur_bit = 0; 
+	    m_wpointer = &m_frame[0];
+            *m_wpointer = 0;
+	    m_cur_len = 0; m_cur_bit = 0; 
 	  }
 	}
       }
-      else
-      {
+      else {
         //cout << "normal data reception "<< endl;	
         // data reception
-	*_wpointer =  ((*_wpointer) )+((uint64_t)(peer->get_serial())<<cur_bit);
-	//cout << "partial word received " << std::bitset<10> (*_wpointer)  << endl;
-	//peer->get_serial();
-	cur_bit++;	
-#if 1
-        if (cur_bit==10)
-        {
-	   //cout << "complete word received " << std::bitset<10> (*_wpointer)  << endl;
-           // all bits in current word have been sent out
-            _wpointer++;
-            *_wpointer = 0;
-           cur_bit = 0;
-	   cur_len++;
-	   if (cur_len == payload_length) 
-	   {
-	     headcd = 50; // 50 bit of header to be read
-	     cout << "complete data packet received " << cur_len << endl;
-             for (int i=0;i<cur_len;i++) cout << _frame[i] << " " ; 
+	*m_wpointer =  ((*m_wpointer) )+((uint64_t)(m_peer.get_serial())<<m_cur_bit);
+	//cout << "partial word received " << std::bitset<10> (*m_wpointer)  << endl;
+	//m_peer->get_serial();
+	m_cur_bit++;	
+        if (m_cur_bit==10) {
+	  //cout << "complete word received " << std::bitset<10> (*m_wpointer)  << endl;
+          // all bits in current word have been sent out
+          m_wpointer++;
+          *m_wpointer = 0;
+          m_cur_bit = 0;
+	  m_cur_len++;
+	  if (m_cur_len == payload_length)  {
+	     m_headcd = 50; // 50 bit of header to be read
+	     cout << "complete data packet received " << m_cur_len << endl;
+             for (int i=0;i<m_cur_len;i++) cout << m_frame[i] << " " ; 
              cout << endl;
-	     handle_packet(curhead,payload_length,_frame);
+	     handle_packet(m_curhead,payload_length,m_frame);
 	   }
          }
-#endif	
-      }  
+       }  
     }
-    else
-    {
-      syncpos++;
-      curhead = ((curhead >>1) + (((uint64_t)(peer->get_serial()))<<49)) & 0x3ffffffffffff;
-      //cout << syncpos << " "<< std::bitset<50>( curhead )<< " " <<
-      //std::bitset<50> (synchead) << endl;
-      if (curhead == synchead)
-      {
-        // cout << syncpos << " "<< std::bitset<50>( curhead )<< "-"   << std::hex <<
-	//curhead << "payload length " << payload_length << endl;
-        issync= true;
-	// cout <<"receiver: Synchronisation trouvee au bit" << (syncpos-50) << endl;   
-	headcd = 50; // 50 bit of header to be read
+    else {
+      m_syncpos++;
+      m_curhead = ((m_curhead >>1) + (((uint64_t)(m_peer.get_serial()))<<49)) & 0x3ffffffffffff;
+      if (m_curhead == m_synchead) {
+        // cout << m_syncpos << " "<< std::bitset<50>( m_curhead )<< "-"   << std::hex <<
+	//m_curhead << "payload length " << payload_length << endl;
+        m_is_synced= true;
+	cout <<"receiver: Synchronisation trouvee au bit" << (m_syncpos-50) << endl;   
+	m_headcd = 50; // 50 bit of header to be read
 #ifdef STATS
-        packetcount_by_type[sampa_head().get_packet_type(curhead)]++;
+        packetcount_by_type[sampa_head().get_packet_type(m_curhead)]++;
 	packetcount++;
 #endif	
       }	
@@ -194,8 +178,7 @@ void receiver::display_stats()
 {
   cout << "Received packets statistics" << endl;
   cout << "Total received packets : " << packetcount<< endl;
-  for (int i=0;i<8;i++)
-  {
+  for (int i=0;i<8;i++) {
     cout <<  "Received packets type "<<i<< " : " << packetcount_by_type[i]<< endl; packetcount_by_type[i]=0;
   }
 }
--- src/receiver.h
+++ src/receiver.h
@@ -16,8 +16,8 @@ class receiver
 {
 public:
   receiver();
-  receiver(elink *p);
-  receiver(int port,GBT_r *p);
+  receiver(elink &p);
+  receiver(int port,GBT_r &p);
   void set_userhandler(void (*foo)(int,int,int,int,int,short *));
   void set_userhandler(receiver_handler* handler);
   void start();
@@ -33,25 +33,26 @@ private:
   receiver_handler     *rec_handl;
 
   void handle_packet(const uint64_t header,int len,uint16_t *buffer);
-  uint64_t synchead;
-  uint64_t curhead;
+  
+  uint64_t m_synchead;
+  uint64_t m_curhead;
  
-  elink    *peer;
-  bool     issync;
-  int      syncpos;  
-  int      headcd;
-  int      payload_length;
-  int       cur_bit;
-  uint16_t  cur_len;
-  uint16_t  *_wpointer;
-  uint16_t  _frame[1024];
+  elink     &m_peer;
+  bool      m_is_synced;
+  int       m_syncpos;  
+  int       m_headcd;
+  int       m_cur_bit;
+  uint16_t  m_cur_len;
+  uint16_t  *m_wpointer;
+  uint16_t  m_frame[1024];
   
   std::thread  *TheThread;
+  int       m_port;
   
   // statistic records
 #ifdef STATS
-  uint32_t  packetcount;
-  uint32_t  packetcount_by_type[8];
+  uint32_t  m_packetcount;
+  uint32_t  m_packetcount_by_type[8];
 #endif  
 };
 #endif
