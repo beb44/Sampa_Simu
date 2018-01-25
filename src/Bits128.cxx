67447cec44b7a854f3eb674fd17f51bbed74518c
beb44 <blaine@numericable.fr>
2018-01-25 16:29:38 +0100

    improve Bits128 performance

--- src/Bits128.cxx
+++ src/Bits128.cxx
@@ -2,6 +2,25 @@
 /*!
  * \brief O 128 bit constructor
  */
+ const uint32_t Bits128::maskcomp[32] = 
+           { 0xFFFFFFFE,0xFFFFFFFD,0xFFFFFFFB,0xFFFFFFF7,
+             0xFFFFFFEF,0xFFFFFFDF,0xFFFFFFBF,0xFFFFFF7F, 
+             0xFFFFFEFF,0xFFFFFDFF,0xFFFFFBFF,0xFFFFF7FF, 
+             0xFFFFEFFF,0xFFFFDFFF,0xFFFFBFFF,0xFFFF7FFF, 
+             0xFFFEFFFF,0xFFFDFFFF,0xFFFBFFFF,0xFFF7FFFF, 
+             0xFFEFFFFF,0xFFDFFFFF,0xFFBFFFFF,0xFF7FFFFF, 
+             0xFEFFFFFF,0xFDFFFFFF,0xFBFFFFFF,0xF7FFFFFF, 
+             0xEFFFFFFF,0xDFFFFFFF,0xBFFFFFFF,0x7FFFFFFF};
+ const uint32_t Bits128::mask[32] = 
+           { 0x00000001,0x00000002,0x00000004,0x00000008,
+             0x00000010,0x00000020,0x00000040,0x00000080, 
+             0x00000100,0x00000200,0x00000400,0x00000800, 
+             0x00001000,0x00002000,0x00004000,0x00008000, 
+             0x00010000,0x00020000,0x00040000,0x00080000, 
+             0x00100000,0x00200000,0x00400000,0x00800000, 
+             0x01000000,0x02000000,0x04000000,0x08000000, 
+             0x10000000,0x20000000,0x40000000,0x80000000}; 
+	     
 
 Bits128::Bits128()
 {
@@ -16,6 +35,7 @@ Bits128::Bits128()
  * \param m0 less significant 32 bits
  *
  */
+ #if 0
 Bits128::Bits128(uint32_t m3,uint32_t m2,uint32_t m1,uint32_t m0)
 {
     w[0]= m0;
@@ -23,28 +43,11 @@ Bits128::Bits128(uint32_t m3,uint32_t m2,uint32_t m1,uint32_t m0)
     w[2]= m2;
     w[3]= m3;
 }
+#endif
 /*!
  * \brief return bit value at pos
  *
  * \param pos bit position 
  *
- * \return bit value
- */
-
-uint8_t Bits128::Get(int pos)
-{
-  return  (w[pos/32] >> (pos %32))& 1;
-}
-
-/*!
- * \brief set bit value at pos
- *
- * \param pos bit position 
- * \param val value of the bit
- *
- */
-void Bits128::Set(int pos,uint8_t val)
-{
-  w[pos/32] = w[pos/32] & ~(1<<(pos %32)); // clear bit
-  w[pos/32] = w[pos/32] | (val<<(pos %32)); // set value
-}
+ * \return bit value    
+ */  
--- src/Bits128.h
+++ src/Bits128.h
@@ -4,17 +4,56 @@
 /*!
  * \brief 128 bits word emulation class
  */
+ 
 
 class Bits128
 {
 public:
   Bits128();
-  Bits128(uint32_t m3,uint32_t m2,uint32_t m1,uint32_t m0);
-  uint8_t Get(int pos);
-  void Set(int pos,uint8_t val);
-private: 
+  Bits128(uint32_t m3,uint32_t m2,uint32_t m1,uint32_t m0)
+  {
+    w[0]= m0;
+    w[1]= m1;
+    w[2]= m2;
+    w[3]= m3;
+  };
+  /*!
+   * \brief return bit value at pos
+   *
+   * \param pos bit position 
+   *
+   * \return bit value
+   */  
+  uint8_t Get(int pos)
+  {
+    return  ((w[pos/32] & mask[pos %32])?1:0);	     
+  };
+  /*!
+   * \brief set bit value at pos
+   *
+   * \param pos bit position 
+   * \param val value of the bit
+   *
+   */
+  void Set(int pos,uint8_t val)
+  {
+    int P1= pos>>5;
+    int P2= pos & 0x1f;
+
+    w[P1] &=  maskcomp[P2];
+    if (val)
+    {
+      w[P1] |= mask[P2];
+    }
+    //w[P1] |=  (val<<P2); // set value
+
+  };
   
+private: 
+   static const  uint32_t  maskcomp[32];
+   static const  uint32_t  mask[32];
   /*! \brief word value split in 4 32 bit maord                      */
   uint32_t  w[4];
 };
-#endif
+
+ #endif
